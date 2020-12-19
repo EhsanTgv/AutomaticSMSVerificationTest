c5013c233bb7b3ead096264609a8c27984cf70a3
EhsanTgv <EhsanTgv@gmail.com>
2020-12-19 23:21:21 +0330

    Obtain Phone Number

--- app/src/main/java/com/taghavi/automaticsmsverification/MainActivity.kt
+++ app/src/main/java/com/taghavi/automaticsmsverification/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.taghavi.automaticsmsverification
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.HintRequest
+import com.google.android.gms.common.api.GoogleApiClient
+
 
 class MainActivity : AppCompatActivity() {
+    val RESOLVE_HINT = 1234
+   lateinit var apiClient: GoogleApiClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun requestHint() {
+        val hintRequest = HintRequest.Builder()
+            .setPhoneNumberIdentifierSupported(true)
+            .build()
+        val intent = Auth.CredentialsApi.getHintPickerIntent(
+            apiClient, hintRequest
+        )
+        startIntentSenderForResult(
+            intent.intentSender,
+            RESOLVE_HINT, null, 0, 0, 0
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESOLVE_HINT) {
+            if (resultCode == RESULT_OK) {
+                val credential: Credential = data.getParcelableExtra(Credential.EXTRA_KEY)!!
+            }
+        }
     }
 }
